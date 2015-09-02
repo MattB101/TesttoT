9eb990a049380ac9abb80e8e7be83eb60ac3270f
MattB101 <mblessing33@gmail.com>
2015-09-01 18:37:51 -0600

    reter

--- ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yet another")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yet another")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0908337c-c5bf-4e20-859f-8e29215111ae")]
+[assembly: Guid("fd089f92-a07a-4a9c-9c04-8135f9e5ab7d")]
 
 // Version information for an assembly consists of the following four values:
 //
